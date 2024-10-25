e6e7455b4fcbf77a47556332c8ffe77aaaaa2472
lichiea <130731273+lichiea@users.noreply.github.com>
2024-10-25 12:49:25 +0300

    Create Data.cpp

---
+++ Data.cpp
@@ -0,0 +1,14 @@
+#include "Data.h"
+// используемые пространства имен
+using namespace glm;
+using namespace std;
+// список графических объектов
+vector<GraphicObject> graphicObjects;
+// используемая камера
+Camera camera;
+// функция для инициализации всех общих данных (камера, объекты и т.д.)
+void initData()
+{
+// инициализация графических объектов
+...
+}
