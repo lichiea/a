293dd469364f501b81d0b8ec1df0ac84bde14070
lichiea <130731273+lichiea@users.noreply.github.com>
2024-10-25 12:43:50 +0300

    Create Camera.h

---
+++ Camera.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "GL/freeglut.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+using namespace glm;
+
+
+// КЛАСС ДЛЯ РАБОТЫ С КАМЕРОЙ
+class Camera
+{
+public:
+	// конструкторы
+	Camera();
+	Camera(vec3 position);
+	Camera(float x, float y, float z);
+	// установка и получение позиции камеры
+	void setPosition(vec3 position);
+	vec3 getPosition();
+	float getAngleX();
+	float getAngleY();
+	float getR();
+	//функции для перемещения камеры
+	void rotateLeftRight(float degree);
+	void rotateUpDown(float degree);
+	void zoomInOut(float distance);
+	// функция для установки матрицы камеры
+	void apply();
+	~Camera() {};
+private:
+	// перерасчет позиции камеры после поворотов
+	void recalculatePosition();
+private:
+	// радиус и углы поворота
+	float r;
+	float angleX;
+	float angleY;
+	// позиция камеры
+	vec3 position;
+};
