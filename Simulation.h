713f23b5cf5929a70ca6f94e972d17ae06f21db9
lichiea <130731273+lichiea@users.noreply.github.com>
2024-10-25 12:44:50 +0300

    Create Simulation.h

---
+++ Simulation.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "GL/freeglut.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <functional>
+using namespace glm;
+
+
+    // Установка и получение позиции объекта
+    void simulation();
