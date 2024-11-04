c3a83e9bc06c7a3f077924d363ea0acfcb7f0fb8
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-04 19:47:42 +0300

    Update Data5.cpp

--- aokg/Data/Data5.cpp
+++ aokg/Data/Data5.cpp
@@ -2,9 +2,6 @@
 //// используемые пространства имен
 using namespace glm;
 using namespace std;
-
-
-
 LARGE_INTEGER frequency;  // Частота таймера
 LARGE_INTEGER lastCounter; // Последний отсчет
 float simulationTime; // Время симуляции
@@ -14,7 +11,8 @@ int fps;            // Количество FPS
 LARGE_INTEGER lastFPSCounter; // Последний отсчет для FPS
 
 //// список графических объектов
-vector<GraphicObject> graphicObjects;
+vector<GraphicObject*> graphicObjects;
+vector<PhongMaterial> mats;
 //// используемая камера
 Camera cam;
 Light light;
@@ -25,50 +23,32 @@ PhongMaterial material;
 void initData()
 {
 
-    // задание всех параметров одного материала
-    PhongMaterial material;
-    //material.setDiffuse(vec4(1.0, 0.0, 0.0, 1.0));
-    //material.setAmbient(vec4(0.4, 0.0, 0.0, 1.0));
-    //material.setSpecular(vec4(0.0, 0.0, 0.0, 1.0));
-    //material.setEmission(vec4(0.0, 0.0, 0.0, 1.0));
-    //material.setShininess(64.0);
-    //material.apply();
-
-    GraphicObject tempGraphicObject;
     initializeTiming();
     cam.setPosition(vec3(20, 15, 17.5));
     light.setPosition(vec4(20.0,20.0,15.0,1.0));
-    // 1 -----------------------------------------
-    material.load("data//materials//material_1.txt");
-    material.apply();
-    tempGraphicObject.setPosition(vec3(-5, 0, 0));
-    tempGraphicObject.setAngle(0.0);
-    tempGraphicObject.setСolor(vec3(1, 0, 0));
-    tempGraphicObject.setMaterial(&material);
-    graphicObjects.push_back(tempGraphicObject);
-    // 2 -----------------------------------------
-    //material.load("data//materials//material_2.txt");
-    //material.apply();
-    tempGraphicObject.setPosition(vec3(5, 0, 0));
-    tempGraphicObject.setAngle(180.0);
-    tempGraphicObject.setСolor(vec3(1, 0, 0));
-    //tempGraphicObject.setMaterial(&material);
-    graphicObjects.push_back(tempGraphicObject);
-    // 3 -----------------------------------------
-    //material.load("data//materials//material_3.txt");
-    //material.apply();
-    tempGraphicObject.setPosition(vec3(0, 0, 5));
-    tempGraphicObject.setAngle(90.0);
-    tempGraphicObject.setСolor(vec3(1, 0, 0));
-    //tempGraphicObject.setMaterial(&material);
-    graphicObjects.push_back(tempGraphicObject);
-    // 4 -----------------------------------------
-    //material.load("data//materials//material_4.txt");
-    //material.apply();
-    tempGraphicObject.setPosition(vec3(0, 0, -5));
-    tempGraphicObject.setAngle(270.0);
-    tempGraphicObject.setСolor(vec3(1, 0, 0));
-    //tempGraphicObject.setMaterial(&material);
-    graphicObjects.push_back(tempGraphicObject);
+    light.setAmbient(vec4( 0.4, 0.0, 0.0, 1.0 ));
+    light.setDiffuse(vec4(1.0, 0.0, 0.0, 1.0));
+    light.setSpecular (vec4(1.0, 1.0, 1.0, 1.0));
+    for (int i = 0; i < 4; ++i) {
+        float ug;
+        vec3 temp;
+        if (i == 0) { ug = 0; temp = vec3(-5, 0, 0);}
+        if (i == 1){ug = 180; temp = vec3(5, 0, 0);}
+        if (i == 2) { ug = 90; temp = vec3(0, 0, 5);}
+        if (i == 3) { ug = 270; temp = vec3(0, 0, -5); }
+        // Создаем новый объект GraphicObject
+        GraphicObject* tempGraphicObject = new GraphicObject();
+        PhongMaterial* material = new PhongMaterial();
+        // Загрузка материала
+        material->load("data//materials//material_" +to_string(i+1) + ".txt");
 
+        // Установка параметров для каждого объекта
+        tempGraphicObject->setPosition(temp);
+        tempGraphicObject->setAngle(ug);
+        tempGraphicObject->setСolor(vec3(1, 1, 1));
+        tempGraphicObject->setMaterial(material);
+
+        // Добавляем объект в вектор
+        graphicObjects.push_back(tempGraphicObject);
+    }
 }
