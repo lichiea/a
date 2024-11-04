ccb6063693c7e19e6cad7252bdfb335a193532ba
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-04 21:03:43 +0300

    Update GO5.cpp

--- aokg/GraphicObject/GO5.cpp
+++ aokg/GraphicObject/GO5.cpp
@@ -2,32 +2,32 @@
 
 GraphicObject::GraphicObject() {};
 
-void GraphicObject::setPosition(vec3 position_temp){
+void GraphicObject::setPosition(vec3 position_temp) {
 	position = position_temp;
 	recalculateModelMatrix();
 };
 
-vec3 GraphicObject::getPosition(){return position;}
+vec3 GraphicObject::getPosition() { return position; }
 
 void GraphicObject::setAngle(float grad) {
 	angle = grad;
 	recalculateModelMatrix();
 };
-float GraphicObject::getAngle(){return angle;}
+float GraphicObject::getAngle() { return angle; }
 
-void GraphicObject::setСolor(vec3 color_temp) {color = color_temp;};
-vec3 GraphicObject::getColor() {return color;}
+void GraphicObject::setСolor(vec3 color_temp) { color = color_temp; };
+vec3 GraphicObject::getColor() { return color; }
 
-void GraphicObject::setMaterial(PhongMaterial* materialk) { material = materialk; };
-PhongMaterial* GraphicObject::getMaterial() { return material; };
+void GraphicObject::setMaterial(shared_ptr<PhongMaterial> materialk) { material = materialk; };
+shared_ptr<PhongMaterial> GraphicObject::getMaterial() { return material; };
 
 // расчет матрицы modelMatrix на основе position и angle
-void GraphicObject::recalculateModelMatrix(){
-	mat4 model=mat4(1.0);
+void GraphicObject::recalculateModelMatrix() {
+	mat4 model = mat4(1.0);
 	// Перевод
 	model = translate(model, getPosition());
 	// Вращение вокруг
-	model = rotate(model, radians(getAngle()),vec3(0.0,1.0,0.0));
+	model = rotate(model, radians(getAngle()), vec3(0.0, 1.0, 0.0));
 	// Сохранение матрицы в массив GLfloat
 	for (int i = 0; i < 16; ++i) {
 		float value = model[i / 4][i % 4];
@@ -40,10 +40,10 @@ void GraphicObject::recalculateModelMatrix(){
 
 }
 // вывести объект
-void GraphicObject::draw(){
+void GraphicObject::draw() {
 	vec3 c = getColor();
 	glColor3f(c.r, c.g, c.b);
-	PhongMaterial* material = getMaterial();
+	shared_ptr<PhongMaterial> material = getMaterial();
 	if (material != nullptr) {
 		material->apply();
 	}
