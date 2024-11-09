b0f85e75da785b184914ccc78c738327b45d877b
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-09 21:40:33 +0300

    Update Data6.h

--- aokg/Data/Data6.h
+++ aokg/Data/Data6.h
@@ -7,6 +7,7 @@
 #include"Mesh.h"
 #include "Camera.h"
 #include "GraphicObject.h"
+#include "GameObject.h"
 #include "Simulation.h"
 #include "Light.h"
 #include "PhongMaterial.h"
@@ -19,11 +20,20 @@ extern int frameCount;       // Счетчик кадров
 extern int fps;            // Количество FPS
 extern LARGE_INTEGER lastFPSCounter; // Последний отсчет для FPS
 //// список графических объектов
-extern std::vector<GraphicObject*> graphicObjects;
+extern std::vector<shared_ptr<GraphicObject>> graphicObjects;
+// используемые меши
+extern vector<shared_ptr<Mesh>> meshes;
+extern vector<shared_ptr<PhongMaterial>> materials;
+// список игровых объектов расположенных на карте
+extern shared_ptr<GameObject> mapObjects[21][21];
+// графический объект для плоскости (частный случай)
+extern GraphicObject planeGraphicObject;
 //// используемая камера
 extern Camera cam;
 extern Light light;
 extern PhongMaterial material;
-extern Mesh m;
+extern Mesh mesh;
 //// функция для инициализации всех общих данных (камера, объекты и т.д.)
 void initData();
+
+extern int passabilityMap[21][21];
