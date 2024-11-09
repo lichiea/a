dcc95eb7091b5df448aa8d67c8055fbb63171cf2
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-09 11:39:08 +0300

    Create Data6.h

---
+++ aokg/Data/Data6.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+#include "GL/freeglut.h"
+#include <glm/glm.hpp>
+#include<memory>
+#include"Mesh.h"
+#include "Camera.h"
+#include "GraphicObject.h"
+#include "Simulation.h"
+#include "Light.h"
+#include "PhongMaterial.h"
+
+extern LARGE_INTEGER frequency;  // Частота таймера
+extern LARGE_INTEGER lastCounter; // Последний отсчет
+extern float simulationTime; // Время симуляции
+extern float frameTime;      // Время между кадрами
+extern int frameCount;       // Счетчик кадров
+extern int fps;            // Количество FPS
+extern LARGE_INTEGER lastFPSCounter; // Последний отсчет для FPS
+//// список графических объектов
+extern std::vector<GraphicObject*> graphicObjects;
+//// используемая камера
+extern Camera cam;
+extern Light light;
+extern PhongMaterial material;
+extern Mesh m;
+//// функция для инициализации всех общих данных (камера, объекты и т.д.)
+void initData();
