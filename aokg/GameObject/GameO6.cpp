a8694bb202016b3e312a7651f6c46ab68ef92c22
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-09 21:41:15 +0300

    Update GameO6.cpp

--- aokg/GameObject/GameO6.cpp
+++ aokg/GameObject/GameO6.cpp
@@ -5,14 +5,19 @@ GameObject::GameObject() {};
 
 // установка используемого графического объекта
 // происходит копирование переданного объекта для последующего использования
-void GameObject::setGraphicObject(const GraphicObject& graphicObject) {};
+void GameObject::setGraphicObject(shared_ptr<GraphicObject> graphicObjectk) { this->graphicObject = graphicObjectk; };
 
 // установка логических координат (два перегруженных метода для удобства)
-void  GameObject::setPosition(int x, int y) {};
-void  GameObject::setPosition(ivec2 position) {};
+void  GameObject::setPosition(int x, int y) { ivec2 p(x, y); this->position = p; };
+void  GameObject::setPosition(ivec2 positionk) { this->position = positionk; };
 
 // получение текущих логических координат
 ivec2  GameObject::getPosition() { return position; };
 
 // вывод игрового объекта на экран
-void  GameObject::draw(void) {};
+void  GameObject::draw(void) {
+    if (graphicObject) {
+        graphicObject->setPosition(vec3(position.x, 0, position.y));
+        graphicObject->draw();
+    }
+};
