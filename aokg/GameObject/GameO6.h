3d7ab8ba3abbe1f8bed6f82961958ebc86921411
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-09 21:41:59 +0300

    Update GameO6.h

--- aokg/GameObject/GameO6.h
+++ aokg/GameObject/GameO6.h
@@ -1,51 +1,34 @@
 #pragma once
-
 #include <windows.h>
-#include <stdio.h>
 #include <iostream>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include "GL/freeglut.h"
-#include "PhongMaterial.h"
-#include<memory>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-using namespace glm;
-
-// КЛАСС ДЛЯ ПРЕДСТАВЛЕНИЯ ОДНОГО ГРАФИЧЕСКОГО ОБЪЕКТА
-class GraphicObject {
+#include <vector>
+#include <string>
+#include "GraphicObject.h"
+// КЛАСС ДЛЯ ПРЕДСТАВЛЕНИЯ ИГРОВОГО ОБЪЕКТА
+class GameObject
+{
 public:
-    // Конструктор
-    GraphicObject();
-    // Установка и получение позиции объекта
-    void setPosition(vec3 position_temp);
-    vec3 getPosition();
-    // Установка и получения угла поворота в градусах
-    // поворот осуществляется в горизонтальной плоскости
-    // вокруг оси Oy по часовой стрелке
-    void setAngle(float grad);
-    float getAngle();
-    // Установка текущего цвета объекта
-    void setСolor(vec3 color_temp);
-    vec3 getColor();
-    // Установка используемого материала
-    void setMaterial(std::shared_ptr<PhongMaterial> material);
-    shared_ptr<PhongMaterial>getMaterial();
-    // Вывести объект
-    void draw();
-    ~GraphicObject() {};
+	// конструктор
+	GameObject();
+	// установка используемого графического объекта
+	// происходит копирование переданного объекта для последующего использования
+	void setGraphicObject(shared_ptr<GraphicObject> graphicObjectk);
+	// установка логических координат (два перегруженных метода для удобства)
+	void setPosition(int x, int y);
+	void setPosition(ivec2 position);
+	// получение текущих логических координат
+	ivec2 getPosition();
+	// вывод игрового объекта на экран
+	void draw(void);
 private:
-    // Позиция объекта в глобальной системе координат
-    vec3 position;
-    // Угол поворота в горизонтальной плоскости (в градусах)
-    float angle;
-    // Цвет модели
-    vec3 color;
-    // Матрица модели (расположение объекта) - чтоб не вычислять каждый раз
-    GLfloat modelMatrix[16];
-    std::shared_ptr<PhongMaterial> material;
-private:
-    // расчет матрицы modelMatrix на основе position и angle
-    void recalculateModelMatrix();
+	// логические координаты игрового объекта
+	ivec2 position;
+	// графический объект (для вывода на экран)
+	shared_ptr<GraphicObject> graphicObject;
 };
