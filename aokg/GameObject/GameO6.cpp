2f73e931395c3ac4e73e6427c3fd7111e6f00ab7
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-13 20:57:03 +0300

    Update GameO6.cpp

--- aokg/GameObject/GameO6.cpp
+++ aokg/GameObject/GameO6.cpp
@@ -1,14 +1,22 @@
 #include"GameObject.h"
 
+#include"GameObject.h"
+
 // конструктор
-GameObject::GameObject() {};
+GameObject::GameObject() { this->setPosition({ 0,0 }); }
 
 // установка используемого графического объекта
 // происходит копирование переданного объекта для последующего использования
-void GameObject::setGraphicObject(shared_ptr<GraphicObject> graphicObjectk) { this->graphicObject = graphicObjectk; };
+void GameObject::setGraphicObject(const GraphicObject& graphicObjectk) {
+    this->graphicObject = graphicObjectk; 
+    this->graphicObject.setPosition({ this->position.x-10,0,this->position.y-10 });
+};
 
 // установка логических координат (два перегруженных метода для удобства)
-void  GameObject::setPosition(int x, int y) { ivec2 p(x, y); this->position = p; };
+void  GameObject::setPosition(int x, int y) { 
+    ivec2 p(x, y); 
+    this->position = p; 
+};
 void  GameObject::setPosition(ivec2 positionk) { this->position = positionk; };
 
 // получение текущих логических координат
@@ -16,8 +24,8 @@ ivec2  GameObject::getPosition() { return position; };
 
 // вывод игрового объекта на экран
 void  GameObject::draw(void) {
-    if (graphicObject) {
-        graphicObject->setPosition(vec3(position.x, 0, position.y));
-        graphicObject->draw();
-    }
+    
+        //graphicObject->setPosition(vec3(position.x - 10, 0, position.y - 10));
+        this->graphicObject.draw();
+    
 };
