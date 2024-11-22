cd3c7e87732fecbdbae2ced6e55a40fa5c9f5188
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-22 15:22:52 +0300

    Create Data6dop.cpp

---
+++ aokg/Data/Data6dop.cpp
@@ -0,0 +1,166 @@
+#include "Data.h"
+
+//// используемые пространства имен
+using namespace glm;
+using namespace std;
+
+//MazeGenerator lab;
+
+void makeMap(int** matrix, int rows, int columns){
+    int arr[21][21] = {0};
+
+    auto maze = MazeGenerator::generate(20, 20);
+
+    //MazeGenerator::print(maze);
+
+    for (int j = 0; j < 20; j++) {
+        for (int i = 0; i < 20; i++) {
+            arr[i][j] = maze[i][j];
+        }
+    }
+
+    // Устанавливаем границы
+    for (int i = 0; i < 21; ++i) {
+        arr[0][i] = 3;
+        arr[21 - 1][i] = 3;
+        arr[i][0] = 3;
+        arr[i][21 - 1] = 3;
+    }
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < columns; c++)
+            matrix[r][c] = arr[r][c];
+}
+
+int passabilityMap[21][21]= {
+3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+3,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,2,0,0,0,3, //
+3,0,2,1,2,0,2,0,2,2,2,1,2,0,2,0,2,0,2,2,3, //
+3,0,2,0,2,0,0,0,2,0,2,0,0,0,2,0,1,0,0,0,3, //
+3,0,1,0,2,2,1,2,2,0,2,0,2,2,2,1,2,0,2,0,3, //
+3,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,2,0,2,0,3, //
+3,0,2,2,1,1,2,0,2,0,2,2,2,2,2,0,2,2,2,0,3, //
+3,0,2,0,0,0,2,0,2,0,0,0,0,0,2,0,0,0,0,0,3, //
+3,0,2,0,2,2,2,0,2,0,2,2,1,2,2,2,1,2,2,0,3, //
+3,0,0,0,2,0,0,0,2,0,2,0,0,0,0,0,0,0,1,0,3, //
+3,2,2,2,2,0,2,2,2,0,2,0,2,2,2,2,2,2,2,0,3, //
+3,0,0,0,2,0,0,0,1,0,2,0,0,0,2,0,0,0,0,0,3, //
+3,0,2,0,2,2,2,0,2,1,2,0,2,2,2,0,2,2,2,2,3, //
+3,0,2,0,0,0,2,0,0,0,2,0,0,0,2,0,2,0,0,0,3, //
+3,2,2,2,2,0,2,2,2,0,2,2,2,0,1,0,2,2,2,0,3, //
+3,0,0,0,0,0,2,0,2,0,0,0,2,0,1,0,0,0,2,0,3, //
+3,0,2,0,2,1,2,0,2,0,2,2,2,0,2,2,2,0,2,0,3, //
+3,0,1,0,1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,3, //
+3,0,2,1,2,0,2,2,2,2,2,0,2,0,2,0,2,2,2,2,3, //
+3,0,0,0,0,0,0,0,0,0,0,0,2,0,2,0,0,0,0,0,3, //
+3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
+};
+
+LARGE_INTEGER frequency;  // Частота таймера
+LARGE_INTEGER lastCounter; // Последний отсчет
+float simulationTime; // Время симуляции
+float frameTime;      // Время между кадрами
+int frameCount;       // Счетчик кадров
+int fps;            // Количество FPS
+LARGE_INTEGER lastFPSCounter; // Последний отсчет для FPS
+
+// список игровых объектов расположенных на карте
+shared_ptr<GameObject> mapObjects[21][21];
+// графический объект для плоскости (частный случай)
+GraphicObject planeGraphicObject;
+GameObject gobj;
+//// список графических объектов
+vector<shared_ptr<GraphicObject>> graphicObjects;
+vector<shared_ptr<PhongMaterial>> materials;
+// используемые меши
+vector<shared_ptr<Mesh>> meshes;
+//// используемая камера
+Camera cam;
+Light light;
+Mesh mesh;
+// задание всех параметров одного материала
+PhongMaterial material;
+
+//// функция для инициализации всех общих данных (камера, объекты и т.д.)
+void initData()
+{
+    // выделяем память под матрицу
+    int** matrix = new int* [21];
+    for (int row = 0; row < 21; row++)
+        matrix[row] = new int[21];
+    makeMap(matrix, 21, 21);
+    initializeTiming();
+    cam.setPosition(vec3(30, 15, 17.5));
+    light.setPosition(vec4(20.0,20.0,15.0,1.0));
+    light.setAmbient(vec4(1.0, 1.0, 1.0, 1.0 ));
+    light.setDiffuse(vec4(1.0, 1.0, 1.0, 1.0));
+    light.setSpecular (vec4(1.0, 1.0, 1.0, 1.0));
+
+    // Создаем новый объект GraphicObject
+    GraphicObject tempGraphicObject1, tempGraphicObject2, tempGraphicObject3;
+
+    shared_ptr<PhongMaterial> material = make_shared<PhongMaterial>();
+    shared_ptr<Mesh> meshp = make_shared<Mesh>();
+
+    //11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    shared_ptr<PhongMaterial> material1 = make_shared<PhongMaterial>();
+    material1->load("data//materials//material_2.txt");
+    shared_ptr<Mesh> mesh1 = make_shared<Mesh>();
+    mesh1->load("data//meshes//ChamferBox.obj");
+
+    tempGraphicObject1.setMaterial(material1);
+    tempGraphicObject1.setMesh(mesh1);
+    meshes.push_back(mesh1);
+    materials.push_back(material1);
+    graphicObjects.push_back(make_shared<GraphicObject>(tempGraphicObject1));
+
+    //22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    shared_ptr<PhongMaterial> material2 = make_shared<PhongMaterial>();
+    material2->load("data//materials//material_3.txt");
+    shared_ptr<Mesh> mesh2 = make_shared<Mesh>();
+    mesh2->load("data//meshes//Box.obj");
+ 
+    tempGraphicObject2.setMaterial(material2);
+    tempGraphicObject2.setMesh(mesh2);
+    meshes.push_back(mesh2);
+    materials.push_back(material2);
+    graphicObjects.push_back(make_shared<GraphicObject>(tempGraphicObject2));
+
+    //33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+    shared_ptr<PhongMaterial> material3 = make_shared<PhongMaterial>();
+    shared_ptr<Mesh> mesh3 = make_shared<Mesh>();
+    material3->load("data//materials//material_4.txt");
+    mesh3->load("data//meshes//Box.obj");
+    tempGraphicObject3.setMaterial(material3);
+    tempGraphicObject3.setMesh(mesh3);
+    meshes.push_back(mesh3);
+    materials.push_back(material3);
+    graphicObjects.push_back(make_shared<GraphicObject>(tempGraphicObject3));
+
+    //4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    material->load("data//materials//material_1.txt");
+    meshp->load("data//meshes//SimplePlane.obj");
+    planeGraphicObject.setMaterial(material);
+    planeGraphicObject.setMesh(meshp);
+    planeGraphicObject.setPosition(vec3(0.0, -0.5, 0.0));
+    meshes.push_back(meshp);
+    materials.push_back(material);
+    for (int i = 0; i < 21; i++) {
+           for (int j = 0; j < 21; j++) {
+               shared_ptr<GameObject> obj = make_shared<GameObject>();
+               obj->setPosition(i, j);
+                if (matrix[i][j] == 1) {
+                    obj->setGraphicObject(tempGraphicObject1);
+                }
+                if (matrix[i][j] == 2) {
+                    obj->setGraphicObject(tempGraphicObject2);
+                }
+                if (matrix[i][j] == 3) {
+                    obj->setGraphicObject(tempGraphicObject3);
+                }
+                mapObjects[i][j] = obj;                
+            }
+        }
+    for (int row = 0; row < 21; row++)
+        delete[] matrix[row];
+    delete[] matrix;
+}
