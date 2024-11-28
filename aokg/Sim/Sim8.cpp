2e43fb9e190352d063cc1710d01730ada843f39e
lichiea <130731273+lichiea@users.noreply.github.com>
2024-11-28 10:47:40 +0300

    Create Sim8.cpp

---
+++ aokg/Sim/Sim8.cpp
@@ -0,0 +1,43 @@
+#include"Simulation.h"
+
+// Инициализация времени
+void initializeTiming() {
+    QueryPerformanceFrequency(&frequency); // Получаем частоту таймера
+    QueryPerformanceCounter(&lastCounter);  // Получаем начальный отсчет
+    QueryPerformanceCounter(&lastFPSCounter); // Получаем начальный отсчет для FPS
+    frameCount = 0; // Инициализация счетчика кадров
+}
+
+// Функция для получения времени симуляции
+float getSimulationTime() {
+    LARGE_INTEGER currentCounter;
+    QueryPerformanceCounter(&currentCounter); // Получаем текущий отсчет
+
+    // Вычисляем время, прошедшее с последнего вызова
+    float deltaTime = static_cast<float>(currentCounter.QuadPart - lastCounter.QuadPart) / frequency.QuadPart;
+
+    // Обновляем последний отсчет
+    lastCounter = currentCounter;
+
+    return deltaTime; // Возвращаем прошедшее время
+}
+
+// Функция симуляции - вызывается максимально часто
+void simulation() {
+    // Определение времени симуляции
+    simulationTime = getSimulationTime();
+
+    gameObjectSimulation(simulationTime);
+    movePlayer(simulationTime);
+ 
+    // Устанавливаем признак того, что окно нуждается в перерисовке
+    glutPostRedisplay();
+}
+
+
+void gameObjectSimulation(float ttime) {
+
+}
+void movePlayer(float ttime) {
+    player->simulate(simulationTime);
+}
